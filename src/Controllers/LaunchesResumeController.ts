5e6269795c245496274fafa0d2c030a4ddad0df7
Johnny Saymon <git@saymon.dev>
2023-02-22 18:29:31 -0300

    refactor(launches): sort by date

--- src/Controllers/LaunchesResumeController.ts
+++ src/Controllers/LaunchesResumeController.ts
@@ -1,5 +1,4 @@
 import { LaunchesRepository, launchesRepositorySymbol } from "../Repositories/LaunchesRepository";
-import { Launch } from "../Models/Launch";
 import { Request, Response } from "express";
 import { inject, injectable } from "inversify";
 
@@ -13,17 +12,25 @@ export class LaunchesResumeController
     public run(request: Request, response: Response): void
     {
         Promise.all([
-            this.launchesRepository.getLasted(),
-            this.launchesRepository.getNext(),
             this.launchesRepository.getPast(),
             this.launchesRepository.getUpcoming(),
         ]).then(
-            (values) => response.json({
-                latest: values[0],
-                next: values[1],
-                past: values[2],
-                upcoming: values[3],
-            })
+            function(values){
+                const launchesPast = values[0]
+                const launchesUpcoming = values[1]
+
+                launchesPast.sort((launchA, launchB) => launchA.date < launchB.date ? 1 : -1)
+                const launchLatest = launchesPast.shift()
+                launchesUpcoming.sort((launchX, launchY) => launchX.date < launchY.date ? 1 : -1)
+                const launchNext = launchesUpcoming.pop()
+
+                response.json({
+                    latest: launchesPast,
+                    next: launchNext,
+                    past: launchLatest,
+                    upcoming: launchesUpcoming,
+                })
+            }
         ).catch(
             error =>  response.status(500).json({
                 message: error.message
